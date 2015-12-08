e2c11682efefcf4b563dca896bc0a9d2168dcb31
lpatiny <gb252243>
2015-12-08 22:43:40 +0100

    Allow to charge the peptide based on a pH

--- src/chargePeptide.js
+++ src/chargePeptide.js
@@ -1,48 +1,44 @@
 'use strict';
 
-var aa = require('./aminoAcids');
+var getAA = require('./getAA');
 
 // SOURCE: https://en.wikipedia.org/wiki/Amino_acid
 
 function chargePeptide(mf, options) {
     var options=options || {};
-    if (options === undefined) options.pH=0;
+    if (options.pH === undefined) options.pH=0;
     var pH=options.pH;
     // we will allow to charge the peptide at a specific pH
 
     // first amino acids (N-terminal)
     if (mf.match(/^H[A-Z][a-z]{2}/)) {
         var firstAA=mf.replace(/^H([A-Z][a-z]{2}).*/,"$1");
-        if (aa[firstAA] && pH<aa[firstAA].pKaN) {
-            mf=mf.replace(/^H([^+])/, 'H+H$1');;
+        if (getAA(firstAA) && pH<getAA(firstAA).pKaN) {
+            mf=mf.replace(/^H([^+])/, 'H+H$1');
         }
-        console.log(firstAA);
     }
 
     // last amino acids (C-terminal)
     if (mf.match(/[A-Z][a-z]{2}OH$/)) {
         var lastAA=mf.replace(/.*([A-Z][a-z]{2})OH$/,"$1");
-        if (aa[firstAA] && pH>aa[firstAA].pKaC) {
-
+        if (getAA(firstAA) && pH>getAA(firstAA).pKaC) {
+            mf=mf.replace(/OH$/, 'O-');
         }
-        console.log(lastAA);
     }
 
     // basic AA
-    mf=mf.replace(/(Arg)(?!\()/g, '$1(H+)');
-    mf=mf.replace(/(His)(?!\()/g, '$1(H+)');
-    mf=mf.replace(/(Lys)(?!\()/g, '$1(H+)');
+    if (pH < getAA('Arg').sc.pKa) mf=mf.replace(/(Arg)(?!\()/g, '$1(H+)');
+    if (pH < getAA('His').sc.pKa) mf=mf.replace(/(His)(?!\()/g, '$1(H+)');
+    if (pH < getAA('Lys').sc.pKa) mf=mf.replace(/(Lys)(?!\()/g, '$1(H+)');
 
     // acid AA
-    mf=mf.replace(/(Asp)(?!\()/g, '$1(H-1-)');
-    mf=mf.replace(/(Glu)(?!\()/g, '$1(H-1-)');
+    if (pH > getAA('Asp').sc.pKa) mf=mf.replace(/(Asp)(?!\()/g, '$1(H-1-)');
+    if (pH > getAA('Glu').sc.pKa) mf=mf.replace(/(Glu)(?!\()/g, '$1(H-1-)');
 
-    mf=mf.replace(/(Cys)(?!\()/g, '$1(H-1-)');
+    if (pH > getAA('Cys').sc.pKa) mf=mf.replace(/(Cys)(?!\()/g, '$1(H-1-)');
 
     return mf;
 };
-
-
 
 module.exports = chargePeptide;
 
