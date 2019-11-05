84216367a31ccf4c364dc3676b063ba7d6db5e82
Luc Patiny <luc@patiny.com>
2019-11-05 11:52:43 +0100

    chore: add more tests for convertAASequence

--- src/__tests__/convertAASequence.test.js
+++ src/__tests__/convertAASequence.test.js
@@ -6,14 +6,25 @@ describe('Checking convert AA sequence', () => {
     expect(result).toEqual('HAlaAlaAlaAlaAlaAlaAlaOH');
   });
 
-test('HAlaAla(H-1OH)AlaOH', () => {
-  const result = convertAASequence('HAlaAla(H-1OH)AlaOH');
-  expect(result).toEqual('HAlaAla(H-1OH)AlaOH');
-})
+  test('HAlaAla(H-1OH)AlaOH', () => {
+    const result = convertAASequence('HAlaAla(H-1OH)AlaOH');
+    expect(result).toEqual('HAlaAla(H-1OH)AlaOH');
+  });
+  test('(C33H37O6N3Si) GluTyrGluLys(C16H30O)GluTyrGluOH', () => {
+    const result = convertAASequence(
+      '(C33H37O6N3Si) GluTyrGluLys(C16H30O)GluTyrGluOH'
+    );
+    expect(result).toEqual('(C33H37O6N3Si) GluTyrGluLys(C16H30O)GluTyrGluOH');
+  });
 
   test('(Me)AAAAAAA(NH2)', () => {
     const result = convertAASequence('(Me)AAAAAAA(NH2)');
     expect(result).toEqual('(Me)AlaAlaAlaAlaAlaAlaAla(NH2)');
+  });
+
+  test('HAlaGlyProOH', () => {
+    const result = convertAASequence('HAlaGlyProOH');
+    expect(result).toEqual('HAlaGlyProOH');
   });
 
   test('ALA SER LYS GLY PRO', () => {
